c9441b5e0d938257d6bc93fb0c5cec177f34f08a
User <User@DESKTOP-802SV85>
2022-04-20 19:35:21 +0300

    lb_3 done

--- Project13/Source.cpp
+++ Project13/Source.cpp
@@ -1,8 +1,80 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
+    setlocale(0, "");
+    int M;
+    cout << "Введите размер массива: ";
+    cin >> M;
+    int* a = new int[M];
 
+    cout << "Введите значения массива: ";
+    for (int i = 0; i < M; i++)
+    {
+        cin >> a[i];
+    }
+
+    while (true)
+    {
+        int n;
+        cout << "\nMenu:" << endl;
+        cout << "1 -Удалить четные значения массива" << endl;
+        cout << "2 - Удалить нечетные значения массива" << endl;
+        cout << "3 - Показать текущий массив" << endl;
+        cout << "4 - Выход" << endl;
+        cin >> n;
+        switch (n)
+        {
+        case 1:
+        {
+            for (int i = 0; i < M; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    for (int j = i; j < M - 1; j++)
+                    {
+                        a[j] = a[j + 1];
+                    }
+                    M--;
+                    i--;
+                }
+            }
+        } break;
+        case 2:
+        {
+            for (int i = 0; i < M; i++)
+            {
+                if (!(a[i] % 2 == 0))
+                {
+                    for (int j = i; j < M - 1; j++)
+                    {
+                        a[j] = a[j + 1];
+                    }
+                    M--;
+                    i--;
+                }
+            }
+        } break;
+        case 3:
+        {
+            cout << "Массив: ";
+            for (int i = 0; i < M; i++)
+            {
+                cout << a[i] << " ";
+            }
+        } break;
+
+        case 4:
+        {
+            delete[] a;
+            return 0;
+        } break;
+        default:
+        {
+            cout << "\nОшибка. Пожалуйста, введите правильный номер" << endl;
+        }
+        }
+    }
 }
